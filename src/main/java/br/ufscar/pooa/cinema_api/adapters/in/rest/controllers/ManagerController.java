6481b264724cca8f38aac18e3806254da6f3cb34
Eduardo Malagutti <eduardosm2004@gmail.com>
2025-07-01 23:44:38 -0300

    refactor: update package path in ManagerController for consistency with rest adapter structure

--- src/main/java/br/ufscar/pooa/cinema_api/adapters/in/rest/controllers/ManagerController.java
+++ src/main/java/br/ufscar/pooa/cinema_api/adapters/in/rest/controllers/ManagerController.java
@@ -1,4 +1,4 @@
-package br.ufscar.pooa.cinema_api.adapters.in.presentation.controllers;
+package br.ufscar.pooa.cinema_api.adapters.in.rest.controllers;
 
 import br.ufscar.pooa.cinema_api.application.dtos.manager.ManagerResponseDTO;
 import br.ufscar.pooa.cinema_api.application.dtos.manager.RegisterManagerRequestDTO;
